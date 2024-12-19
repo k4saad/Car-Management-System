bf3a804b451687ae963213951d70027eba03b2f0
saad khan <saadkhan108b@gmail.com>
2024-12-19 22:53:00 +0530

    first commit

---
+++ src/main/java/com/Car_Management_System/Car_Management_System/CarManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Car_Management_System.Car_Management_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Car_Management_System/Car_Management_System/CarManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Car_Management_System.Car_Management_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
